4201361ac73a068cce50ffc7d0748598f52d3f0f
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-06-02 12:44:12 -0500

    improve player ghosting code + fade when ghosting

--- game/player.gd
+++ game/player.gd
@@ -1,13 +1,19 @@
 extends KinematicBody2D
 
+const speed := 300.0
+
 var max_health := 5.0
 var health := max_health
 
-const speed := 300.0
+var ghosting := false # if the player can take damage
+const ghosting_time := 1.5
 
 onready var health_bar := $HealthBar
-onready var damage_timeout := create_damage_timeout()
 onready var controller := $PhysicsController
+onready var sprite := $IdleAnimation
+
+func _process(_delta: float) -> void:
+	sprite.modulate.a = 0.5 if ghosting else 1.0
 
 func _physics_process(_delta: float) -> void:
 	for i in get_slide_count():
@@ -15,21 +21,17 @@ func _physics_process(_delta: float) -> void:
 		if collision.collider is Enemy:
 			damage()
 
-func create_damage_timeout() -> Timer:
-	var timer := Timer.new()
-	timer.wait_time = 1.5
-	timer.one_shot = true
-	add_child(timer)
-	return timer
-
 func move(direction: Vector2) -> void:
 	controller.velocity = direction * speed
 
 func damage() -> void:
-	if not damage_timeout.is_stopped(): return
+	if ghosting: return
 	
-	damage_timeout.start()
 	health = max(health - 1, 0)
 	health_bar.rect_scale.x = health / max_health
+	
+	ghosting = true
+	yield(get_tree().create_timer(ghosting_time), "timeout")
+	ghosting = false
 
 	# todo: end game, erase self from computer, set computer on fire, end planet

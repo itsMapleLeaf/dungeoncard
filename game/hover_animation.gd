9e2f3da86981a124cb3d13e8eb9b7dce615b7b99
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-06-05 23:31:46 -0500

    type safety!!!

--- game/hover_animation.gd
+++ game/hover_animation.gd
@@ -5,7 +5,7 @@ export var speed := 3
 export var extents := 6
 export var time_offset := 0.0
 
-onready var parent := get_parent()
+onready var parent := get_parent() as Node2D
 onready var offset_y: float = parent.position.y
 
 var time := 0.0

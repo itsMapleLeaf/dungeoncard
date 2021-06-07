5367893db1b73a3a86f9b3733702a25011510868
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-06-07 02:57:37 -0500

    cleanup

--- game/game.gd
+++ game/game.gd
@@ -1,115 +1,119 @@
 extends Node
 
-const card_width := 50
-const card_height := card_width
-const card_spacing := 4
-const card_animation_snappiness := 10
-const hand_size := 5
-const required_attack_distance := 32
-const map_scale := 32
+const field_size := Vector2(5, 5)
+const platform_separation := Vector2(190, 130)
+	
+var entity_manager := EntityManager.new()
 
-onready var deck := $UI/Deck
-onready var hand := $UI/Hand
+var player: EntityManager.Entity
 
-var map_size := 7
+onready var platform_grid := $PlatformGridContainer/PlatformGrid as GridContainer
+onready var world := $World as Node2D
+onready var hand := $Hand as HBoxContainer
 
-onready var player: Player = preload("res://game/player.tscn").instance()
+onready var deck := []
 
 func _ready() -> void:
 	randomize()
-
-	# init player
-	player.map_pos = random_map_position()
-	add_child(player)
 	
-	# init enemies
-	for i in 5:
-		var enemy = preload("res://game/slime.tscn").instance()
-		enemy.map_pos = random_map_position()
-		add_child(enemy)
-
-	# init cards
-	for card in deck.get_children():
-		card.get_button().connect("pressed", self, "play_card", [card])
+	platform_grid.add_constant_override("hseparation", platform_separation.x)
+	platform_grid.add_constant_override("vseparation", platform_separation.y)
 	
-	for _i in range(5):
+	for i in Helpers.points_within_rect(Rect2(Vector2(), field_size)):
+		create_platform()
+	
+	var enemy_spawn_positions := Helpers.points_within_rect(Rect2(Vector2(2, 0), field_size - Vector2(2, 0)))
+	enemy_spawn_positions.shuffle()
+	for i in 3:
+		spawn_enemy(enemy_spawn_positions.pop_front())
+
+	spawn_player(Vector2(0, floor(field_size.y / 2)))
+	
+	build_deck()
+	deck.shuffle()
+	for i in 3:
 		draw_card()
 
-
 func _process(delta: float) -> void:
-	var cards := hand.get_children()
-	cards.sort_custom(self, "sort_by_sort_order")
+	for i in entity_manager.entities:
+		var entity := i as EntityManager.Entity
+		var node := entity.node
+		if node is Player:
+			node.animate_screen_position(get_screen_pos(entity.field_pos), delta)
+		if node is Slime:
+			node.global_position = get_screen_pos(entity.field_pos)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey: if event.is_pressed(): match event.scancode:
+		KEY_UP: move_player(Vector2.UP)
+		KEY_DOWN: move_player(Vector2.DOWN)
+		KEY_LEFT: move_player(Vector2.LEFT)
+		KEY_RIGHT: move_player(Vector2.RIGHT)
+		KEY_SPACE: (player.node as Player).play_attack_animation()
+
+func create_platform() -> void:
+	var platform := preload("res://game/platform.tscn").instance() as Control
+	platform_grid.add_child(platform)
+
+func spawn_enemy(field_pos: Vector2) -> void:
+	var slime := preload("res://game/slime.tscn").instance() as Slime
+	world.add_child(slime)
+	entity_manager.add_at(field_pos, slime)
+
+func spawn_player(field_pos: Vector2) -> void:
+	var player_node := preload("res://game/player.tscn").instance() as Player
+	world.add_child(player_node)
+	player = entity_manager.add_at(field_pos, player_node)
+	player_node.animate_screen_position(get_screen_pos(field_pos), 1)
+
+func get_screen_pos(field_pos: Vector2) -> Vector2:
+	return platform_grid.rect_position + field_pos * platform_separation
+
+func move_player(delta: Vector2) -> void:
+	var new_pos := (player.field_pos + delta)
+	if entity_manager.is_occupied(new_pos): return
 	
-	var hand_width := cards.size() * (card_width + card_spacing) - card_spacing
-	var hand_center := float(hand_width) / 2
+	player.field_pos = Vector2(
+		clamp(new_pos.x, 0, field_size.x - 1),
+		clamp(new_pos.y, 0, field_size.y - 1)
+	)
 	
-	for index in range(hand.get_child_count()):
-		var card: Card = cards[index]
-		var x := index * (card_width + card_spacing) - hand_center
-		var y := -card_height
-		card.rect_position = lerp(card.rect_position, Vector2(x, y), delta * card_animation_snappiness)
+func try_attack():
+	(player.node as Player).play_attack_animation()
+	for dir in [Vector2.UP, Vector2.RIGHT, Vector2.DOWN, Vector2.LEFT]:
+		var ent := entity_manager.get_entity_at_position(player.field_pos + dir)
+		if ent != null and ent.node is Slime:
+			entity_manager.remove(ent)
+			break
 
+func build_deck():
+	for type in Card.get_card_types():
+		var card := preload("res://game/card.tscn").instance() as Card
+		card.set_type(type)
+		card.connect("clicked", self, "play_card", [card])
+		deck.append(card)
 
-func random_map_position() -> Vector2:
-	return Vector2(randi() % map_size, randi() % map_size)
-
-func damage_player() -> void:
-	player.damage()
-
-func play_card(card: Card) -> void:
-	var intents := card.get_intents()
-	for intent in intents: handle_intent(intent)
+func draw_card():
+	deck.shuffle()
+	var card := deck.pop_front() as Card
+	hand.add_child(card)
+	card.play_reveal_animation()
 	
-	# need to make sure the card is out of the hand before draw_card()
-	# so that the positioning stuff in draw_card has the correct card count
-	hand.remove_child(card)
+func play_card(card: Card):
+	match card.type:
+		Card.CardType.MOVE_LEFT:
+			move_player(Vector2.LEFT)
+		Card.CardType.MOVE_RIGHT:
+			move_player(Vector2.RIGHT)
+		Card.CardType.MOVE_UP:
+			move_player(Vector2.UP)
+		Card.CardType.MOVE_DOWN:
+			move_player(Vector2.DOWN)
+		Card.CardType.ATTACK:
+			try_attack()
+
 	draw_card()
-	deck.add_child(card)
+	hand.remove_child(card)
+	deck.append(card)
 
 
-func draw_card() -> void:
-	if deck.get_child_count() == 0: return
-	var card: Card = deck.get_children()[randi() % deck.get_child_count()]
-	
-	Helpers.reparent(card, deck, hand)
-	
-	var cards := hand.get_children()
-	cards.sort_custom(self, "sort_by_sort_order")
-	
-	var hand_width := cards.size() * (card_width + card_spacing) - card_spacing
-	var hand_center := float(hand_width) / 2
-	
-	var index := cards.find(card)
-	var x := index * (card_width + card_spacing) - hand_center
-	var y := -card_height
-	
-	# make the card start out of screen and animate in
-	card.rect_position = Vector2(x, y) + Vector2(0, card_height)
-
-func handle_intent(intent: Object) -> void:
-	if intent is MovementIntent:
-		player.move(intent.get_direction_vector())
-	if intent is AttackIntent:
-		player.try_attack()
-
-func get_closest(nodes: Array, pivot: Node2D) -> Node2D:
-	if nodes.empty(): return null
-
-	var closest: Node2D = nodes.front()
-	var closest_dist := distance_between(closest, pivot)
-	for node in nodes.slice(1, nodes.size()):
-		if not (node is Node2D):
-			continue
-		
-		var distance := distance_between(node, pivot)
-		if distance < closest_dist:
-			closest = node
-			closest_dist = distance
-			
-	return closest
-
-func distance_between(a: Node2D, b: Node2D) -> float:
-	return (a.global_position - b.global_position).length()
-
-func sort_by_sort_order(a: Card, b: Card) -> bool:
-	return a.sort_order < b.sort_order
--- game/helpers.gd
+++ game/helpers.gd
@@ -7,3 +7,11 @@ static func random_item(array: Array):
 static func reparent(child: Node, old_parent: Node, new_parent: Node) -> void:
 	old_parent.remove_child(child)
 	new_parent.add_child(child)
+
+static func points_within_rect(rect: Rect2) -> Array:
+	var points := []
+	
+	for x in rect.size.x: for y in rect.size.y:
+		points.append(rect.position + Vector2(x, y))
+	
+	return points
